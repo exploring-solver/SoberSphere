fd56c16a724903f6642cd12f25382a79b999d574
exploring-solver <amansharma12607@gmail.com>
2024-01-04 01:16:55 +0530

    initial commit

--- SoberSphere_Dashboard/src/components/Signout.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Signout = () => {
-  return <div>Signout</div>;
-};
-
-export default Signout;
--- SoberSphere_Dashboard/src/layouts/dashboard/data/reportsBarChartData.js
+++
@@ -1,4 +0,0 @@
-export default {
-  labels: ["M", "T", "W", "T", "F", "S", "S"],
-  datasets: { label: "Hours", data: [3, 2, 1, 3, 2, 6, 4] },
-};
